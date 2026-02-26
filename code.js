317781bc0b8896192d7f66aae9f38b59a441ef2c
Yusuke Ho <yhowtcc@my.waketech.edu>
2026-02-26 14:06:35 -0500

    help

--- code.js
+++ code.js
@@ -1,26 +1,38 @@
-const d = new Date();
-let time = d.getSeconds() - d.getSeconds()
-on = true
+let time = 0;
+let on = true
 
-
-document.getElementById("startTimer").addEventListener("click", startt);
-document.getElementById("stopTimer").addEventListener("click", stopt);
+document.getElementById("startTimer").addEventListener("click", command);
+document.getElementById("stopTimer").addEventListener("click", command);
 document.getElementById("resetTimer").addEventListener("click", resett);
 
 function startt() {
-    while (on == true) {
-        document.getElementById("stopwatch").innerHTML = time++;
-        //setTimeout(startt,1000);
-        
-    }    
+    on = true;
+    console.log(time);
+    document.getElementById("stopwatch").innerHTML = time++;
+
 }
+
 
 function stopt() {
     on = false
-    
+    console.log(time)
+    document.getElementById("stopwatch").innerHTML = time;
 }
+
 
 function resett() {
     document.getElementById("stopwatch").innerHTML = time -= time;
     
 }
+
+
+
+function command() {
+    if (on == true) {
+        setTimeout(startt, 1000);
+    } else if (on == false) {
+        setTimeout(stopt, 1000);
+
+    }
+}
+
